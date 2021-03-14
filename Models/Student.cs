7a39d7c503aaa2a3d59257ddc4c8518e16dde2b2
Matt Hooper <matt.j.hooper@googlemail.com>
2021-03-14 14:44:21 +0000

    Update Student to DDD approach

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
-        public int ID { get; set; }
-        public string LastName { get; set; }
-        public string FirstMidName { get; set; }
-        public DateTime EnrollmentDate { get; set; }
+        private List<Enrollment> _enrollments;
 
-        public ICollection<Enrollment> Enrollments { get; set; }
+        public int ID { get; private set; }
+        public string LastName { get; private set; }
+        public string FirstMidName { get; private set; }
+        public DateTime EnrollmentDate { get; private set; }
+
+        public IEnumerable<Enrollment> Enrollments => _enrollments.ToList();
+
+        private Student() { }
+
+        public Student(string lastName, string firstMidName, DateTime enrollmentDate)
+        {
+            LastName = lastName;
+            FirstMidName = firstMidName;
+            EnrollmentDate = enrollmentDate;
+        }
     }
 }
